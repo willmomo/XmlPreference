09387f1dd0cfd5e668ea35846d4e9c935de41dd4
Ryota Kojima <ginna.strike@gmail.com>
2016-06-28 12:00:18 +0900

    add new files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test8 {
+	class Program {
+		static void Main(string[] args) {
+			Kjm.XmlPreference obj = new Kjm.XmlPreference();
+			for (int i = 0; i < 100; i++) {
+				obj.SetString("option", "key", i.ToString());
+				Console.WriteLine(obj.GetString("option", "key", "-"));
+			}
+			obj.Dispose();
+			obj = null;
+		}
+	}
+}
